de14b35c636287df315912211240bf3e4d5de807
Igor Banaszuk <ibanaszuk@gmail.com>
2023-11-20 22:38:17 -0700

    recovery middleware with tests

--- middleware/recovery/recovery.go
+++ middleware/recovery/recovery.go
@@ -1,1 +1,16 @@
 package recovery
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Description interface{} `json:"description"`
+}
+
+func ErrorHandler() gin.RecoveryFunc {
+	return func(ctx *gin.Context, err any) {
+		response := Response{Description: err}
+		ctx.AbortWithStatusJSON(500, response)
+	}
+}
--- middleware/recovery/recovery_test.go
+++ middleware/recovery/recovery_test.go
@@ -1,1 +1,27 @@
 package recovery_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"random-stuff-service/middleware/recovery"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRecovery(t *testing.T) {
+	router := gin.Default()
+	router.Use(gin.CustomRecovery(recovery.ErrorHandler()))
+
+	router.GET("/", func(ctx *gin.Context) {
+		panic("recovery incoming")
+	})
+
+	request, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	assert.Equal(t, 500, recorder.Code)
+}
